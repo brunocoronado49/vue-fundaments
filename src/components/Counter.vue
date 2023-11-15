12d28b0354fd37802d0a3989a2cfe358ea57a00d
Francisco Rangel <francorangelcoronado@gmail.com>
2023-11-15 09:39:26 -0600

    terminando la seccion de fundamentos de vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,19 +1,69 @@
 <template>
-    <h2>Counter!</h2>
-    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+    <h2>{{ customTitle }}</h2>
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+    <div>
+        <button @click="decrease">- 1</button>
+        <button @click="increase">+ 1</button>
+    </div>
 </template>
 
 <script>
 export default {
-    // name: 'Counter',
+    props: {
+        msg: String,
+        start: {
+            // required: true,
+            type: Number,
+            default: 5,
+            validator(value) {
+                return value > 0
+            }
+        }
+    },
     data() {
         return {
-            counter: 10
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquare() {
+            return this.counter * this.counter
+        },
+        increase() {
+            return this.counter++
+        },
+        decrease() {
+            return this.counter--
+        }
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
+        }, 
+        customTitle() {
+            return this.msg || 'Counter'
         }
     }
 }
 </script>
 
 <style>
+
+button {
+    margin: 0 5px;
+    background-color: #64bb87;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
 
 </style>
